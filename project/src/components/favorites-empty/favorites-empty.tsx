0cb8e9f738353d55f55a5460e893e63b9715f160
Dmitry_T <cheko.as@inbox.ru>
2022-08-02 17:56:44 +0300

    Добавит реализацию показа страницы favorites-empty

---
+++ project/src/components/favorites-empty/favorites-empty.tsx
@@ -0,0 +1,33 @@
+import Header from 'components/header/header';
+import { AppRoute } from 'const/const';
+import { Link } from 'react-router-dom';
+
+export default function FavoritesEmpty() {
+  return (
+    <div className="page page--favorites-empty">
+      <Header />
+      <main className="page__main page__main--favorites page__main--favorites-empty">
+        <div className="page__favorites-container container">
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+            </div>
+          </section>
+        </div>
+      </main>
+      <footer className="footer container">
+        <Link className="footer__logo-link" to={AppRoute.Main}>
+          <img
+            className="footer__logo"
+            src="img/logo.svg"
+            alt="6 cities logo"
+            width="64"
+            height="33"
+          />
+        </Link>
+      </footer>
+    </div>
+  );
+}
--- project/src/pages/favorites/favorites-screen.tsx
+++ project/src/pages/favorites/favorites-screen.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { AppRoute, CardClassNames } from 'const/const';
 import { Offer } from 'types/offer';
 import { useAppSelector } from 'hooks';
+import FavoritesEmpty from 'components/favorites-empty/favorites-empty';
 
 type GroupedCities = {
   [key: string]: Offer[]
@@ -23,45 +24,49 @@ export default function FavoritesScreen(): JSX.Element {
 
   const citiesList = Object.keys(groupedCities);
 
-  return(
-    <div className="page">
-      <Header />
-      <main className="page__main page__main--favorites">
-        <div className="page__favorites-container container">
-          <section className="favorites">
-            <h1 className="favorites__title">Saved listing</h1>
-            <ul className="favorites__list">
-              {citiesList.map((city) => (
-                <li key={city} className="favorites__locations-items">
-                  <div className="favorites__locations locations locations--current">
-                    <div className="locations__item">
-                      <Link className="locations__item-link" to={AppRoute.Main}>
-                        <span>{city}</span>
-                      </Link>
+  if (favoriteOffers.length > 0) {
+    return(
+      <>
+        <Header />
+        <main className="page__main page__main--favorites">
+          <div className="page__favorites-container container">
+            <section className="favorites">
+              <h1 className="favorites__title">Saved listing</h1>
+              <ul className="favorites__list">
+                {citiesList.map((city) => (
+                  <li key={city} className="favorites__locations-items">
+                    <div className="favorites__locations locations locations--current">
+                      <div className="locations__item">
+                        <Link className="locations__item-link" to={AppRoute.Main}>
+                          <span>{city}</span>
+                        </Link>
+                      </div>
                     </div>
-                  </div>
-                  <div className="favorites__places">
-                    {groupedCities[city].map((offer) =>(
-                      <Card key={offer.id} offer={offer} className={CardClassNames.Favorites} isSmall />
-                    ))}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
-      </main>
-      <footer className="footer container">
-        <Link className="footer__logo-link" to={AppRoute.Main}>
-          <img
-            className="footer__logo"
-            src="img/logo.svg"
-            alt="6 cities logo"
-            width="64"
-            height="33"
-          />
-        </Link>
-      </footer>
-    </div>
-  );
+                    <div className="favorites__places">
+                      {groupedCities[city].map((offer) =>(
+                        <Card key={offer.id} offer={offer} className={CardClassNames.Favorites} isSmall />
+                      ))}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        </main>
+        <footer className="footer container">
+          <Link className="footer__logo-link" to={AppRoute.Main}>
+            <img
+              className="footer__logo"
+              src="img/logo.svg"
+              alt="6 cities logo"
+              width="64"
+              height="33"
+            />
+          </Link>
+        </footer>
+      </>
+    );
+  }
+
+  return <FavoritesEmpty />;
 }
